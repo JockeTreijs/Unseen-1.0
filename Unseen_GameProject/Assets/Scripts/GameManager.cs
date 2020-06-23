468c06cd421c5a0297d4d6c058ab2ef89b23ab86
JockeTreijs <jocke.treijs@student.forsbergsskola.se>
2020-06-23 11:57:59 +0200

    Version 0.1

---
+++ Unseen_GameProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Tilemap darkMap;
+    public Tilemap blurredMap;
+    public Tilemap backgroundMap;
+
+    public Tile darkTile;
+    public Tile blurredTile;
+
+    private Button startButton;
+    private Button exitButton;
+    private Button mainMenuButton;
+    private Button tryAgainButton;
+    private Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        darkMap.origin = blurredMap.origin = backgroundMap.origin;
+        darkMap.size = blurredMap.size = backgroundMap.size;
+
+        foreach (Vector3Int p in darkMap.cellBounds.allPositionsWithin)
+        {
+            darkMap.SetTile(p, darkTile);
+        }
+
+        foreach (Vector3Int p in blurredMap.cellBounds.allPositionsWithin)
+        {
+            blurredMap.SetTile(p, blurredTile);
+        }
+
+        startButton = GameObject.Find("StartButton").GetComponent<Button>();
+        startButton.onClick.AddListener(StartGame);
+
+        exitButton = GameObject.Find("ExitButton").GetComponent<Button>();
+        exitButton.onClick.AddListener(ExitGame);
+
+        mainMenuButton = GameObject.Find("MainMenuButton").GetComponent<Button>();
+        mainMenuButton.onClick.AddListener(MainMenu);
+
+        tryAgainButton = GameObject.Find("TryAgainButton").GetComponent<Button>();
+        tryAgainButton.onClick.AddListener(StartGame);
+
+        quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
+        quitButton.onClick.AddListener(MainMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void FinishLevel()
+    {
+        SceneManager.LoadScene("End");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
---
+++ Unseen_GameProject/Assets/Scripts/Trap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trap : MonoBehaviour
+{
+    public Transform startPosition;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Loser");
+            //other.gameObject.transform.position = new Vector2(-12.5f, 4.5f);
+        }
+    }
+}
